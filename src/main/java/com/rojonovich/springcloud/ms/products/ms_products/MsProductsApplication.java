6664b1913441b0a01249dc9ef671638fd2417704
programerred92 <haprorold@gmail.com>
2026-05-07 05:54:26 -0500

    Initialize Spring Boot project with Gradle setup and essential configuration files

---
+++ src/main/java/com/rojonovich/springcloud/ms/products/ms_products/MsProductsApplication.java
@@ -0,0 +1,13 @@
+package com.rojonovich.springcloud.ms.products.ms_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductsApplication.class, args);
+	}
+
+}
